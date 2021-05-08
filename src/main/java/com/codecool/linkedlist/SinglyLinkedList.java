6833334135fb9d5f1b2b315a71d433d2c6b10339
Andrassyattila <attilaaa1313@gmail.com>
2021-05-08 18:29:45 +0200

    all test passsed

--- src/main/java/com/codecool/linkedlist/SinglyLinkedList.java
+++ src/main/java/com/codecool/linkedlist/SinglyLinkedList.java
@@ -1,17 +1,17 @@
 package com.codecool.linkedlist;
 
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
 
     private class Link {
 
-        private int value;
+        private T value;
         private Link next;
 
-        Link(int value) {
+        Link(T value) {
             this.value = value;
         }
 
-        int getValue() {
+        T getValue() {
             return value;
         }
 
@@ -25,7 +25,6 @@ public class SinglyLinkedList {
     }
 
     private Link head;
-
     public SinglyLinkedList() {
     }
 
@@ -36,7 +35,19 @@ public class SinglyLinkedList {
      *
      * @param value value to be appended
      */
-    public void add(int value) {
+    public void add(T value) {
+        Link new_node = new Link(value);
+        if (head == null) {
+            head = new_node;
+            return;
+        }
+        new_node.next = null;
+        Link last = head;
+        while (last.next != null) {
+            last = last.next;
+        }
+        last.next = new_node;
+
     }
 
     /**
@@ -45,8 +56,20 @@ public class SinglyLinkedList {
      * @param index the position of requested value
      * @return value of element at index
      */
-    public int get(int index) {
-        return 0;
+    public T get(int index) {
+        int counter = 0;
+        Link link=head;
+        if(index == 0) {
+            return head.getValue();
+        }
+        else if(index>0 && index<size()) {
+            while (counter != index) {
+                link = link.getNext();
+                counter++;
+            }
+            return link.getValue();
+        }
+            throw new IndexOutOfBoundsException();
     }
 
     /**
@@ -55,8 +78,19 @@ public class SinglyLinkedList {
      * @param number value to be searched
      * @return Index of 'number' if it's in the list, otherwise -1;
      */
-    public int indexOf(int number) {
-        return 0;
+    public int indexOf(T number) {
+        int index = 0;
+        Link current = head;
+
+        while (current != null) {
+            if (current.getValue().equals(number)) {
+                return index;
+            }
+            index++;
+            current = current.getNext();
+        }
+
+        return -1;
     }
 
     /**
@@ -65,16 +99,63 @@ public class SinglyLinkedList {
      * @param index  Position of the new element
      * @param number Value to be inserted.
      */
-    public void insert(int index, int number) {
-    }
+    public void insert(int index, T number) {
+        if (index < 0) {
+            throw new IndexOutOfBoundsException();
 
+        }else if(size()==0){
+            add(number);
+        }
+        else if (index == 0 ) {
+            Link newNode = new Link(number);
+            newNode.setNext(head);
+            head=newNode;
+        }else if(index >0 &&index<size()){
+            Link node = new Link(number);
+
+            node.next = null;
+
+
+            Link current = this.head;
+            Link previous = null;
+
+            int i = 0;
+
+            while (i < index) {
+                previous = current;
+                current = current.next;
+
+                if (current == null) {
+                    break;
+                }
+
+                i++;
+            }
+
+            node.next = current;
+            previous.next = node;
+        }
+
+        else if (index == size()) {
+            add(number);
+        }
+    }
     /**
      * Returns with the amount of inserted nodes.
      *
      * @return Size of list.
      */
     public int size() {
-        return 0;
+        if(head == null){
+            return 0;
+        }
+        int counter = 1;
+        Link current = head;
+        while (current.next != null) {
+            current = current.next;
+            counter++;
+        }
+        return counter;
     }
 
     /**
